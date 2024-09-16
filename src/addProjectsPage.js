4f22e98c76deec6d7919fcd1dcd35e9517d15008
Alex <alexvnep@gmail.com>
2024-09-16 15:51:05 +0200

    Add DOM manip to render Page to add projects

---
+++ src/addProjectsPage.js
@@ -0,0 +1,30 @@
+const addProjectPage = document.createElement("div");
+
+const addProjectPageHeading = document.createElement("h2");
+addProjectPageHeading.textContent = "Add A Project.";
+addProjectPage.appendChild(addPageHeading);
+
+const projectForm = document.createElement("form");
+addProjectPage.appendChild(projectForm);
+
+const projectTitleLabel = document.createElement("label");
+projectTitleLabel.textContent = "To-Do: ";
+projectTitleLabel.setAttribute("for", "todo-title");
+
+const projectTitleInput = document.createElement("input");
+projectTitleInput.setAttribute("id", "todo-title");
+projectTitleInput.setAttribute("type", "text");
+
+const addNewProjectBtn = document.createElement("button");
+addNewProjectBtn.textContent = "+";
+addNewProjectBtn.setAttribute("type", "submit");
+
+projectForm.appendChild(projectTitleLabel);
+projectForm.appendChild(projectTitleInput);
+projectForm.appendChild(addNewProjectBtn);
+
+function getAddProjectPage() {
+  return addProjectPage;
+}
+
+export { getAddProjectPage, addNewProjectBtn, projectTitleInput };
