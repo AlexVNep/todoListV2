4f402ae9297df8c898ebed4ee3cac5308e42bfe3
Alex <alexvnep@gmail.com>
2024-09-16 13:58:42 +0200

    Add event listener to addButton and the needed imports for it to function and import displayToDo and execute it

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import "./styles.css";
 import header from "./homePage";
+import { addButton } from "./navBar";
+import { getAddPage } from "./addPage";
+import { displayToDo } from "./createToDoContent";
+import { container } from "./homePage";
+
+addButton.addEventListener("click", () => {
+  container.replaceChildren();
+  container.appendChild(getAddPage());
+});
+displayToDo();
