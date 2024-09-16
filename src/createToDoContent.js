2e200df29b9bbe9ee18c666c12a64c80c0354dc2
Alex <alexvnep@gmail.com>
2024-09-16 11:59:27 +0200

    Add DOM manipulation to creat general todos dynamically

---
+++ src/createToDoContent.js
@@ -0,0 +1,30 @@
+import { myTodoList } from "./addToDo";
+
+const container = document.querySelector("#container");
+
+const todoListCardHeading = document.createElement("h2");
+container.appendChild(todoListCardHeading);
+todoListCardHeading.textContent = `General To-Do's`;
+
+const todoListCard = document.createElement("div");
+todoListCard.classList.add("todo-list-card");
+container.appendChild(todoListCard);
+
+const todoUL = document.createElement("ul");
+todoListCard.appendChild(todoUL);
+
+// const todoList = document.createElement("li");
+// todoUL.appendChild(todoList);
+
+function displayToDo() {
+  myTodoList.forEach((todo) => {
+    const todoList = document.createElement("li");
+    todoUL.appendChild(todoList);
+    todoList.textContent = todo.title;
+    console.log(todo.title);
+  });
+}
+
+console.log(myTodoList);
+
+export { container, displayToDo };
