dd031158f840b663a2a5c647f311977f6db08185
Alex <alexvnep@gmail.com>
2024-09-16 15:54:02 +0200

    Rename todayButton to projectsButton and export

--- src/navBar.js
+++ src/navBar.js
@@ -3,8 +3,8 @@ const navContainer = document.createElement("nav");
 const addButton = document.createElement("button");
 addButton.textContent = "Add To-Do";
 
-const todayButton = document.createElement("button");
-todayButton.textContent = "Today";
+const projectsButton = document.createElement("button");
+projectsButton.textContent = " Add Projects";
 
 const upcomingButton = document.createElement("button");
 upcomingButton.textContent = "Upcoming";
@@ -19,10 +19,10 @@ const logbookButton = document.createElement("button");
 logbookButton.textContent = "Logbook";
 
 navContainer.appendChild(addButton);
-navContainer.appendChild(todayButton);
+navContainer.appendChild(projectsButton);
 navContainer.appendChild(upcomingButton);
 navContainer.appendChild(anytimeButton);
 navContainer.appendChild(somedayButton);
 navContainer.appendChild(logbookButton);
 
-export { navContainer, addButton };
+export { navContainer, addButton, projectsButton };
