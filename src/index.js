359fb976865b5c686dbd0b6af3c6f1e65ba96407
Alex <alexvnep@gmail.com>
2024-09-16 15:32:33 +0200

    Add  event listener for addNewTodoBtn to append todo to list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import "./styles.css";
-import header from "./homePage";
+import { header } from "./homePage";
 import { addButton } from "./navBar";
 import { getAddPage } from "./addPage";
 import { displayToDo } from "./createToDoContent";
 import { container } from "./homePage";
+import { addNewTodoBtn } from "./addPage";
+import { getToDoListContainer } from "./createToDoContent";
+import { addToDo } from "./addToDo";
 
 addButton.addEventListener("click", () => {
   container.replaceChildren();
   container.appendChild(getAddPage());
+});
+
+addNewTodoBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  container.replaceChildren();
+  addToDo();
+  container.appendChild(getToDoListContainer());
 });
 displayToDo();
