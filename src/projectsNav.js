f73b45c2c14ca7deb052c56d9bf3992e65c35605
Alex <alexvnep@gmail.com>
2024-09-16 16:39:27 +0200

    Create DOM lists for project titles to display dynamically

--- src/projectsNav.js
+++ src/projectsNav.js
@@ -1,4 +1,6 @@
-const projectsContainer = document.createElement("nav");
+import { myProjects } from "./addProjects";
+
+const projectsContainer = document.createElement("div");
 projectsContainer.classList.add("projects-nav");
 
 const projectHeading = document.createElement("h3");
@@ -6,4 +8,20 @@ projectHeading.textContent = "Projects";
 
 projectsContainer.appendChild(projectHeading);
 
-export { projectsContainer };
+const projectListCard = document.createElement("div");
+projectListCard.classList.add("project-list-card");
+projectsContainer.appendChild(projectListCard);
+
+const projectUL = document.createElement("ul");
+projectListCard.appendChild(projectUL);
+
+function displayProject() {
+  myProjects.forEach((project) => {
+    const projectList = document.createElement("li");
+    projectUL.appendChild(projectList);
+    projectList.textContent = project.title;
+    console.log(project.title);
+  });
+}
+
+export { projectsContainer, displayProject };
