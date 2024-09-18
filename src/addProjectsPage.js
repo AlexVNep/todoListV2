e6fb3042dd7ddb2a95420bcba7546c737eb5f7a1
Alex <alexvnep@gmail.com>
2024-09-18 16:02:11 +0200

    Uncomment and refactor code into a function

--- src/addProjectsPage.js
+++ src/addProjectsPage.js
@@ -1,82 +1,73 @@
-// const addProjectPage = document.createElement("div");
+function getAddProjectPage() {
+  const addProjectPage = document.createElement("div");
 
-// const addProjectPageHeading = document.createElement("h2");
-// addProjectPageHeading.textContent = "Add A Project.";
-// addProjectPage.appendChild(addProjectPageHeading);
+  const addProjectPageHeading = document.createElement("h2");
+  addProjectPageHeading.textContent = "Add A Project.";
+  addProjectPage.appendChild(addProjectPageHeading);
 
-// const projectForm = document.createElement("form");
-// addProjectPage.appendChild(projectForm);
+  const projectForm = document.createElement("form");
+  addProjectPage.appendChild(projectForm);
 
-// const projectTitleLabel = document.createElement("label");
-// projectTitleLabel.textContent = "Title: ";
-// projectTitleLabel.setAttribute("for", "todo-title");
+  const projectTitleLabel = document.createElement("label");
+  projectTitleLabel.textContent = "Title: ";
+  projectTitleLabel.setAttribute("for", "todo-title");
 
-// const projectTitleInput = document.createElement("input");
-// projectTitleInput.setAttribute("id", "todo-title");
-// projectTitleInput.setAttribute("type", "text");
+  const projectTitleInput = document.createElement("input");
+  projectTitleInput.setAttribute("id", "todo-title");
+  projectTitleInput.setAttribute("type", "text");
 
-// const projectDescriptionLabel = document.createElement("label");
-// projectDescriptionLabel.textContent = "Description: ";
-// projectDescriptionLabel.setAttribute("for", "todo-description");
+  const projectDescriptionLabel = document.createElement("label");
+  projectDescriptionLabel.textContent = "Description: ";
+  projectDescriptionLabel.setAttribute("for", "todo-description");
 
-// const projectDescriptionInput = document.createElement("input");
-// projectDescriptionInput.setAttribute("id", "todo-description");
-// projectDescriptionInput.setAttribute("type", "text");
+  const projectDescriptionInput = document.createElement("input");
+  projectDescriptionInput.setAttribute("id", "todo-description");
+  projectDescriptionInput.setAttribute("type", "text");
 
-// const projectDueDateLabel = document.createElement("label");
-// projectDueDateLabel.textContent = "Due Date: ";
-// projectDueDateLabel.setAttribute("for", "todo-duedate");
+  const projectDueDateLabel = document.createElement("label");
+  projectDueDateLabel.textContent = "Due Date: ";
+  projectDueDateLabel.setAttribute("for", "todo-duedate");
 
-// const projectDueDateInput = document.createElement("input");
-// projectDueDateInput.setAttribute("id", "todo-duedate");
-// projectDueDateInput.setAttribute("type", "date");
+  const projectDueDateInput = document.createElement("input");
+  projectDueDateInput.setAttribute("id", "todo-duedate");
+  projectDueDateInput.setAttribute("type", "date");
 
-// const projectPriorityLabel = document.createElement("label");
-// projectPriorityLabel.textContent = "Priority: ";
-// projectPriorityLabel.setAttribute("for", "todo-priority");
+  const projectPriorityLabel = document.createElement("label");
+  projectPriorityLabel.textContent = "Priority: ";
+  projectPriorityLabel.setAttribute("for", "todo-priority");
 
-// const projectPriorityInput = document.createElement("select");
-// projectPriorityInput.setAttribute("id", "todo-priority");
-// projectPriorityInput.setAttribute("name", "priority");
+  const projectPriorityInput = document.createElement("select");
+  projectPriorityInput.setAttribute("id", "todo-priority");
+  projectPriorityInput.setAttribute("name", "priority");
 
-// const optionLow = document.createElement("option");
-// optionLow.textContent = "Low";
-// optionLow.setAttribute("value", "Low");
-// const optionMed = document.createElement("option");
-// optionMed.textContent = "Medium";
-// optionMed.setAttribute("value", "Medium");
-// const optionHigh = document.createElement("option");
-// optionHigh.textContent = "High";
-// optionHigh.setAttribute("value", "High");
+  const optionLow = document.createElement("option");
+  optionLow.textContent = "Low";
+  optionLow.setAttribute("value", "Low");
+  const optionMed = document.createElement("option");
+  optionMed.textContent = "Medium";
+  optionMed.setAttribute("value", "Medium");
+  const optionHigh = document.createElement("option");
+  optionHigh.textContent = "High";
+  optionHigh.setAttribute("value", "High");
 
-// const addNewProjectBtn = document.createElement("button");
-// addNewProjectBtn.textContent = "+";
-// addNewProjectBtn.setAttribute("type", "submit");
+  const addNewProjectBtn = document.createElement("button");
+  addNewProjectBtn.textContent = "+";
+  addNewProjectBtn.setAttribute("type", "submit");
 
-// projectForm.appendChild(projectTitleLabel);
-// projectForm.appendChild(projectTitleInput);
-// projectForm.appendChild(projectDescriptionLabel);
-// projectForm.appendChild(projectDescriptionInput);
-// projectForm.appendChild(projectDueDateLabel);
-// projectForm.appendChild(projectDueDateInput);
-// projectForm.appendChild(projectPriorityLabel);
-// projectForm.appendChild(projectPriorityInput);
-// projectPriorityInput.appendChild(optionLow);
-// projectPriorityInput.appendChild(optionMed);
-// projectPriorityInput.appendChild(optionHigh);
+  projectForm.appendChild(projectTitleLabel);
+  projectForm.appendChild(projectTitleInput);
+  projectForm.appendChild(projectDescriptionLabel);
+  projectForm.appendChild(projectDescriptionInput);
+  projectForm.appendChild(projectDueDateLabel);
+  projectForm.appendChild(projectDueDateInput);
+  projectForm.appendChild(projectPriorityLabel);
+  projectForm.appendChild(projectPriorityInput);
+  projectPriorityInput.appendChild(optionLow);
+  projectPriorityInput.appendChild(optionMed);
+  projectPriorityInput.appendChild(optionHigh);
 
-// projectForm.appendChild(addNewProjectBtn);
+  projectForm.appendChild(addNewProjectBtn);
+  return addProjectPage;
+}
 
-// function getAddProjectPage() {
-//   return addProjectPage;
-// }
-
-// export {
-//   getAddProjectPage,
-//   addNewProjectBtn,
-//   projectTitleInput,
-//   projectForm,
-//   projectDescriptionInput,
-//   projectDueDateInput,
-//   projectPriorityInput,
-// };
+export { getAddProjectPage };
