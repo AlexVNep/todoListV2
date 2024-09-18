0aa984349d251a5d4dedaa024357b37b3cacc29d
Alex <alexvnep@gmail.com>
2024-09-18 15:59:13 +0200

    Uncomment remaining code to get project details to show in content

--- src/projectsListContainer.js
+++ src/projectsListContainer.js
@@ -1,5 +1,4 @@
 import { myProjects } from "./addProjects";
-// import { container } from "./homePage";
 
 function getProjectsContainer() {
   const projectsListContainer = document.createElement("div");
@@ -23,49 +22,50 @@ function getProjectsContainer() {
     projectList.textContent = project.title;
     projectList.setAttribute("id", `Button${[index]}`);
     console.log(project.description);
-    //     projectList.addEventListener("click", () => {
-    //       container.replaceChildren();
-    //       const clickedProjectCard = document.createElement("div");
-    //       const projectTitle = document.createElement("h2");
-    //       projectTitle.textContent = project.title;
+    projectList.addEventListener("click", () => {
+      const container = document.getElementById("content-container");
+      container.replaceChildren();
+      const clickedProjectCard = document.createElement("div");
+      const projectTitle = document.createElement("h2");
+      projectTitle.textContent = project.title;
 
-    //       const projectDescrip = document.createElement("p");
-    //       projectDescrip.textContent = project.description;
+      const projectDescrip = document.createElement("p");
+      projectDescrip.textContent = project.description;
 
-    //       const projectDueDate = document.createElement("p");
-    //       projectDueDate.textContent = project.dueDate;
+      const projectDueDate = document.createElement("p");
+      projectDueDate.textContent = project.dueDate;
 
-    //       const projectPriority = document.createElement("p");
-    //       projectPriority.textContent = project.priority;
+      const projectPriority = document.createElement("p");
+      projectPriority.textContent = project.priority;
 
-    //       const addProjectToDoButton = document.createElement("button");
-    //       addProjectToDoButton.textContent = "Add Tasks";
+      const addProjectToDoButton = document.createElement("button");
+      addProjectToDoButton.textContent = "Add Tasks";
 
-    //       addProjectToDoButton.addEventListener("click", () => {
-    //         const taskCard = document.createElement("div");
-    //         const taskInput = document.createElement("input");
-    //         const taskButton = document.createElement("button");
-    //         taskButton.textContent = "Add Task";
-    //         container.appendChild(taskCard);
-    //         taskCard.appendChild(taskInput);
-    //         taskCard.appendChild(taskButton);
+      addProjectToDoButton.addEventListener("click", () => {
+        const taskCard = document.createElement("div");
+        const taskInput = document.createElement("input");
+        const taskButton = document.createElement("button");
+        taskButton.textContent = "Add Task";
+        container.appendChild(taskCard);
+        taskCard.appendChild(taskInput);
+        taskCard.appendChild(taskButton);
 
-    //         taskButton.addEventListener("click", () => {
-    //           const taskDiv = document.createElement("div");
-    //           taskDiv.textContent = taskInput.value;
+        taskButton.addEventListener("click", () => {
+          const taskDiv = document.createElement("div");
+          taskDiv.textContent = taskInput.value;
 
-    //           container.appendChild(taskDiv);
-    //           container.removeChild(taskCard);
-    //         });
-    //       });
+          container.appendChild(taskDiv);
+          container.removeChild(taskCard);
+        });
+      });
 
-    //       container.appendChild(clickedProjectCard);
-    //       clickedProjectCard.appendChild(projectTitle);
-    //       clickedProjectCard.appendChild(projectDescrip);
-    //       clickedProjectCard.appendChild(projectDueDate);
-    //       clickedProjectCard.appendChild(projectPriority);
-    //       clickedProjectCard.appendChild(addProjectToDoButton);
-    //     });
+      container.appendChild(clickedProjectCard);
+      clickedProjectCard.appendChild(projectTitle);
+      clickedProjectCard.appendChild(projectDescrip);
+      clickedProjectCard.appendChild(projectDueDate);
+      clickedProjectCard.appendChild(projectPriority);
+      clickedProjectCard.appendChild(addProjectToDoButton);
+    });
   });
   return projectsListContainer;
 }
