773b9eb45ec154ba581228b2967f558718724051
Alex <alexvnep@gmail.com>
2024-09-19 09:44:15 +0200

    Add DOM code to append a card for each project and its details, add a delete button

--- src/projectDetails.js
+++ src/projectDetails.js
@@ -1,1 +1,45 @@
+import { addProject, myProjects } from "./addProjects";
 
+function showAllProjects() {
+  const container = document.getElementById("content-container");
+  const allProjectsDiv = document.createElement("div");
+  allProjectsDiv.setAttribute("id", "all-projects-div");
+
+  container.appendChild(allProjectsDiv);
+
+  const allProjectsHeading = document.createElement("h2");
+  allProjectsHeading.textContent = "All Projects";
+  allProjectsDiv.appendChild(allProjectsHeading);
+
+  myProjects.forEach((project) => {
+    const projectCard = document.createElement("div");
+    allProjectsDiv.appendChild(projectCard);
+
+    const projectCardTitle = document.createElement("h3");
+    projectCardTitle.textContent = project.title;
+
+    const projectCardDescription = document.createElement("p");
+    projectCardDescription.textContent = project.description;
+
+    const ProjectCardDueDate = document.createElement("p");
+    ProjectCardDueDate.textContent = project.dueDate;
+
+    const projectCardPriority = document.createElement("p");
+    projectCardPriority.textContent = project.priority;
+
+    const projectCardDeleteButton = document.createElement("button");
+    projectCardDeleteButton.textContent = "DELETE";
+
+    projectCard.appendChild(projectCardTitle);
+    projectCard.appendChild(projectCardDescription);
+    projectCard.appendChild(ProjectCardDueDate);
+    projectCard.appendChild(projectCardPriority);
+    projectCard.appendChild(projectCardDeleteButton);
+  });
+
+  console.log(myProjects);
+
+  return allProjectsDiv;
+}
+
+export { showAllProjects };
