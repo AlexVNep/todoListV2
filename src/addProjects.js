049a28f4f300c08882b043a8efdd78323a406408
Alex <alexvnep@gmail.com>
2024-09-19 08:22:44 +0200

    Remove import bloat and fix addProject func by moving the push to addProjectsPage

--- src/addProjects.js
+++ src/addProjects.js
@@ -1,12 +1,3 @@
-// import {
-//   projectTitleInput,
-//   projectForm,
-//   projectDescriptionInput,
-//   projectDueDateInput,
-//   projectPriorityInput,
-// } from "./addProjectsPage";
-// import { displayProject } from "./projectsNav";
-
 let myProjects = [];
 
 class Project {
@@ -18,45 +9,29 @@ class Project {
   }
 }
 
-const project1 = new Project(
-  "Fix car",
-  "Buy parts for car",
-  "07/22/2025",
-  "Low"
-);
-const project2 = new Project(
-  "Learn web development",
-  "Learn Javascript",
-  "07/22/2025",
-  "High"
-);
-const project3 = new Project(
-  "Work",
-  "Learn React and other stuff",
-  "12/25/2024",
-  "High"
-);
-
-myProjects[0] = project1;
-myProjects[1] = project2;
-myProjects[2] = project3;
-
 function addProject() {
-  myProjects = [];
-  const projectTitle = projectTitleInput.value;
-  const projectDescription = projectDescriptionInput.value;
-  const projectDueDate = projectDueDateInput.value;
-  const projectPriority = projectPriorityInput.value;
-  const newProject = new Project(
-    projectTitle,
-    projectDescription,
-    projectDueDate,
-    projectPriority
+  const project1 = new Project(
+    "Fix car",
+    "Buy parts for car",
+    "07/22/2025",
+    "Low"
   );
-  myProjects.push(newProject);
-  displayProject();
-  projectForm.reset();
-  console.log(newProject);
+  const project2 = new Project(
+    "Learn web development",
+    "Learn Javascript",
+    "07/22/2025",
+    "High"
+  );
+  const project3 = new Project(
+    "Work",
+    "Learn React and other stuff",
+    "12/25/2024",
+    "High"
+  );
+
+  myProjects[0] = project1;
+  myProjects[1] = project2;
+  myProjects[2] = project3;
 }
 
-export { addProject, myProjects };
+export { addProject, myProjects, Project };
